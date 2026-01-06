e95733f02731ab9e96bc58a9d14c7c23eb30bd00
Marouanne13 <tikamarouane@gmail.com>
2026-01-06 19:41:09 +0100

    Update

--- IoT-Climate-Monitor_Django-React-main/frontend/src/api/authApi.js
+++ IoT-Climate-Monitor_Django-React-main/frontend/src/api/authApi.js
@@ -1,78 +1,22 @@
-// api/sensorApi.js
-import axios from "axios";
-import { jwtDecode } from "jwt-decode";
-import { useNavigate } from "react-router-dom";
+import { axiosInstance } from "./axiosInstance";
+import { saveToken } from "./sensorApi";
 
-const API_URL = `${process.env.REACT_APP_API_URL}/api/auth`; 
+export async function loginUser(credentials) {
+  const res = await axiosInstance.post("auth/login/", credentials);
+  saveToken(res.data.access);
+  return res.data;
+}
 
-
-
-// Register
 export async function registerUser(data) {
-  try {
-    const res = await axios.post(`${API_URL}/register`, data);
-    return res.data;
-  } catch (error) {
-    console.error("Erreur register :", error);
-    throw error;
-  }
+  const res = await axiosInstance.post("auth/register/", data);
+  return res.data;
 }
 
-// Login
-export async function loginUser(data) {
-  try {
-    const res = await axios.post(`${API_URL}/login/`, data);
-    saveToken(res.data.access);
-    res.data.user = await fetchCurrentUser();
-    return res.data;
-  } catch (error) {
-    console.error("Erreur login :", error);
-    throw error;
-  }
+export async function fetchCurrentUser() {
+  const res = await axiosInstance.get("auth/me/");
+  return res.data;
 }
 
-export function saveToken(token) {
-  localStorage.setItem("token", token);
+export function logout() {
+  localStorage.clear();
 }
-
-export function saveUser(user) {
-  localStorage.setItem("currentUser", JSON.stringify(user));
-}
-
-export function getToken() {
-  return localStorage.getItem("token");
-}
-
-export const getCurrentUser = () => {
-  const userStr = localStorage.getItem("currentUser");
-  return userStr ? JSON.parse(userStr) : null;
-};
-
-export const fetchCurrentUser = async() => {
-    const token = localStorage.getItem("token");
-    const res = await axios.get(`${API_URL}/me/`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    saveUser(res.data);
-    return res.data;
-};
-
-export function removeToken() {
-  localStorage.removeItem("token");
-}
-export function removeCurrentUser() {
-  localStorage.removeItem("currentUser");
-}
-
-export function useLogout() {
-    const navigate = useNavigate();
-
-    return () => {
-        removeToken();
-        removeCurrentUser();
-        navigate("/login", { replace: true });
-    };
-}
-
