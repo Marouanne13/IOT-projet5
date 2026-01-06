0ed5cefbb4257bc52f60fff9409f2d3698e70527
Marouanne13 <tikamarouane@gmail.com>
2026-01-06 19:40:19 +0100

    Update

--- IoT-Climate-Monitor_Django-React-main/frontend/src/api/axiosInstance.js
+++ IoT-Climate-Monitor_Django-React-main/frontend/src/api/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { getToken } from "./sensorApi"; // ou authApi selon ton nom
+import { getToken } from "./sensorApi";
 
 const API_URL = "https://marouannetika.pythonanywhere.com/api/";
 
@@ -7,10 +7,13 @@ export const axiosInstance = axios.create({
   baseURL: API_URL,
 });
 
-axiosInstance.interceptors.request.use((config) => {
-  const token = getToken();
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
